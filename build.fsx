12a681b4892399a471677de3af80a52713d9acb0
Mathias Fussenegger <f.mathias@zignar.net>
2016-01-04 01:11:46 +0100

    add build script using fake/paket and add .travis.yml

---
+++ build.fsx
@@ -0,0 +1,41 @@
+// include Fake lib
+#r @"packages/FAKE/tools/FakeLib.dll"
+open Fake
+open System
+
+// Properties
+let buildDir = "./build/"
+let solutionFile = "crate-mono.sln"
+
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "Build" (fun _ ->
+    !! "crate-mono.sln"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
+
+Target "RunTests" (fun _ ->
+    !! (buildDir + "/*test.dll")
+    |> NUnit (fun p ->
+        { p with
+            DisableShadowCopy = true
+            TimeOut = TimeSpan.FromMinutes 20.
+            ToolPath = "packages/NUnit.Runners/tools/"
+            OutputFile = "TestResult.xml" })
+)
+
+
+// Dependencies
+"Clean"
+  ==> "Build"
+  ==> "RunTests"
+
+
+// start build
+RunTargetOrDefault "Build"
